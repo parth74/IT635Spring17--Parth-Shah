08bbac19c5b53abce9e9b402c2388df1a063c0c4
parth74 <pds28@njit.edu>
2017-03-05 23:19:28 -0500

    IT 635 Proj

---
+++ IT635_Proj/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title> TechShop </title>
+
+	<link rel="stylesheet" href="style_CSS/style.css" media="all" />
+	</head>
+
+<body>
+
+	<div class="main_layout">
+
+		<div class="header_wrapper">
+		
+			< img id="logo" src="images/logo.png" />
+			< img id="banner" src="images/banner.jpg" />
+
+
+		</div>
+
+		<div class="menubar"> This is menubar  </div>
+		<div class="content_layout">
+
+			<div id="sidebar">This is sidebar </div>
+			<div id="content_area"> This is content area </div>
+
+		</div>
+
+
+			<div id="footer"> This is the footer </div>
+
+	
+
+	</div>
+
+
+
+
+</body>
+
+</html>
