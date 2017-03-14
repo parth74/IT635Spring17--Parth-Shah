08d3fa5ceb3056f2dac5a78e9bafe6aaf1ff138d
parth74 <pds28@njit.edu>
2017-03-13 22:50:00 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title> TechShop </title>
+
+	<link rel="stylesheet" href="style_CSS/style.css" media="all" />
+	</head>
+
+<body>
+
+	<!-- Main layout starts from here -->
+
+	<div class="main_layout">
+
+		<!-- Header starts here -->
+
+		<div class="header_wrapper">
+
+		</div>
+		<!-- Header ends here -->
+		<!-- Navigation Bar starts here -->
+
+		<div class="menubar">  
+
+			<ul id="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Products</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Sign up</a></li>
+				<li><a href="#">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			
+			<ul>
+
+		<div id="form">
+			<form method="get" action="results.php enctype="multipart/form-data">
+				<input type="text" name="user_name" placeholder="search a produt" />
+				<input type="submit" name="search" value="Search" />
+			</form>
+		</div>
+
+		<!-- Navigation Bar ends here -->
+
+		<!-- Content layout starts here-->
+
+		<div class="content_layout" style="text-align:left;">
+
+			<div id="sidebar">
+		<div id="sidebar_title">Categories</div>
+				<ul id="categ">
+				<lil><a href="#">Computers</a></li>
+				<lil><a href="#">Mobiles</a></li>
+				<lil><a href="#">Cameras</a></li>
+				<lil><a href="#">Laptops</a></li>
+				<lil><a href="#">Tablets</a></li>
+				<lil><a href="#">Smart Watch</a></li>
+
+			<ul>
+			<div id="sidebar_title">Brands</div>
+			<ul id="categ">
+				<lil><a href="#">HP</a></li>
+				<lil><a href="#">Dell</a></li>
+				<lil><a href="#">Motorola</a></li>
+				<lil><a href="#">Sony</a></li>
+				<lil><a href="#">LG</a></li>
+				<lil><a href="#">Apple</a></li>
+
+			<ul>
+
+ </div>
+			<div id="content_area"> This is content area </div>
+
+		</div>
+		<!-- Content layout ends here -->
+
+
+			<div id="footer"> This is the footer </div>
+
+	
+
+	</div>
+
+<!-- Main container ends here -->
+
+
+</body>
+
+</html>
