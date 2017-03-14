edafe08d5b2bc2c758fa697758352d106e6de27a
parth74 <pds28@njit.edu>
2017-03-13 22:50:46 -0400

    Add files via upload

--- IT635_Proj/index.php
+++ IT635_Proj/index.php
@@ -8,23 +8,70 @@
 
 <body>
 
+	<!-- Main layout starts from here -->
+
 	<div class="main_layout">
 
-		<div class="header_wrapper">
-		
-			< img id="logo" src="images/logo.png" />
-			< img id="banner" src="images/banner.jpg" />
+		<!-- Header starts here -->
 
+		<div class="header_wrapper">
 
 		</div>
+		<!-- Header ends here -->
+		<!-- Navigation Bar starts here -->
 
-		<div class="menubar"> This is menubar  </div>
-		<div class="content_layout">
+		<div class="menubar">  
 
-			<div id="sidebar">This is sidebar </div>
+			<ul id="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Products</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Sign up</a></li>
+				<li><a href="#">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			
+			<ul>
+
+		<div id="form">
+			<form method="get" action="results.php enctype="multipart/form-data">
+				<input type="text" name="user_name" placeholder="search a produt" />
+				<input type="submit" name="search" value="Search" />
+			</form>
+		</div>
+
+		<!-- Navigation Bar ends here -->
+
+		<!-- Content layout starts here-->
+
+		<div class="content_layout" style="text-align:left;">
+
+			<div id="sidebar">
+		<div id="sidebar_title">Categories</div>
+				<ul id="categ">
+				<lil><a href="#">Computers</a></li>
+				<lil><a href="#">Mobiles</a></li>
+				<lil><a href="#">Cameras</a></li>
+				<lil><a href="#">Laptops</a></li>
+				<lil><a href="#">Tablets</a></li>
+				<lil><a href="#">Smart Watch</a></li>
+
+			<ul>
+			<div id="sidebar_title">Brands</div>
+			<ul id="categ">
+				<lil><a href="#">HP</a></li>
+				<lil><a href="#">Dell</a></li>
+				<lil><a href="#">Motorola</a></li>
+				<lil><a href="#">Sony</a></li>
+				<lil><a href="#">LG</a></li>
+				<lil><a href="#">Apple</a></li>
+
+			<ul>
+
+ </div>
 			<div id="content_area"> This is content area </div>
 
 		</div>
+		<!-- Content layout ends here -->
 
 
 			<div id="footer"> This is the footer </div>
@@ -33,7 +80,7 @@
 
 	</div>
 
-
+<!-- Main container ends here -->
 
 
 </body>
